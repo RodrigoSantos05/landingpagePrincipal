3512c84efe7c9b1f82107bf05c94a142795d18d6
RodrigoSantos05 <134339136+RodrigoSantos05@users.noreply.github.com>
2023-06-20 00:40:49 -0300

    Add files via upload

---
+++ landingpage2/script.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.menu');
+const NaveMenu = ducument.querySelector('.nav-menu');
+
+menu,addEventListener('click', {} => {
+    menu.classList.toggle('ativo');
+    NaveMenu.classList.toggle('ativo');
+})
